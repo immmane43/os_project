adbb613d328a9f6f7084f3e4a9f591b3c8e081c5
immmane43 <immane43@students.rowan.edu>
2023-05-03 23:24:31 -0400

    lab09 & lab10 & lab11

--- kernel.c
+++ kernel.c
@@ -1,21 +1,27 @@
 #include "console.h"
 #include "keyboard.h"
-const char prompt[]="shell>";
+
+
+const char prompt[] = "~>";
+const char banner[] = "Welcome to Nagamani's\nRun 'help' for a list of available commands\n\n";
+
+
+
+
 void main(void) 
 {
-	unsigned char byte;
-	clear_terminal();
-	print_line(" ");
-	print_string_with_color(prompt,BLACK,LIGHT_CYAN);
-	print_string("cursor position:");
-	print_integer(get_cursor_position());
-	print_character('\n');
-        while (1) {
-        update_cursor();
-                while (byte = scan()) {
-
-                        print_character(charmap[byte]);
-                }
+    char command_buf[64], args_buf[128];
+    init_terminal();
+    clear_terminal();
+    print_line(banner);
+    while (1)
+    {
+        print_string_with_color(prompt, BLACK,LIGHT_CYAN);
+        read_command(command_buf,args_buf);
+        if (handle_command(command_buf, args_buf) < 0) 
+        {
+            return;
         }
-	return;
+    }
+    return;
 }
